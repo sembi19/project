bfc853ec1daf6ae8edad46d7a01d556e8476ad53
Ivan <example@gmail.com>
2020-08-18 09:50:54 +0600

    first commit

---
+++ js1/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+//alert('Hello');
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+// const answer = +prompt("Are you over 18?", "18");
+// console.log(answer +5);
+
+// const answer = [];
+// answer[0] = prompt("Whats your name?", '');
+// answer[1] = prompt("Whats your surname?", '');
+// answer[2] = prompt("How old are you?", '');
+
+// document.write(answer);
+
+// console.log(typeof(answer));
+
+const category = 'toys';
+
+console.log(`https:://someurl.com/${category}/5`);
+
+const user = 'Ivan';
+
+alert(`Hello, ${user}`);
